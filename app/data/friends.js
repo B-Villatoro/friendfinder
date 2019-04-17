589a3dd04caf64465e8b01ff70415f693e3dd0ee
Bryan Villatoro <bvillatoro@ymail.com>
2019-04-17 16:26:25 -0400

    form values are accurate

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+let friendArray = [
+    {
+        name: "Friend Guy",
+        photo: "placehold.it/200x200",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+]
+
+module.exports = friendArray;
